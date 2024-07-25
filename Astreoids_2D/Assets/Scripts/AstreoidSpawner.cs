31cf9af1df25de69772ec32b70b6853414cd34e3
dinosOnVenus <ozlemgoksun1389@gmail.com>
2024-07-25 14:47:51 +0300

    .

--- Astreoids_2D/Assets/Scripts/AstreoidSpawner.cs
+++ Astreoids_2D/Assets/Scripts/AstreoidSpawner.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class AstreoidSpawner : MonoBehaviour
 {
+    public Astreoids Astroid_Prefab;
+    public float Spawn_Rate = 1.0f;
+    public int Spawn_Amount = 1;
+    //public float Spawn_Distance =
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        InvokeRepeating(nameof(Spawn), this.Spawn_Rate, this.Spawn_Rate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Spawn()
+    {
+        for (int i = 0; i < this.Spawn_Amount; i++)
+        {
+            //Vector3 Spawn_Direction = Random.insideUnitCircle.normalized * ;
+            //Vector3 Spawn_Point;
+
+            //Astreoids Astreoid_ = Instantiate(this.Astroid_Prefab, position, rotation);
+        }
     }
 }
